a95935dc09f6636e2fa5af427e8fa5f464836f61
darkhanakmyrza <darkhanakmyrza1@gmail.com>
2020-12-04 23:49:15 +0500

    some changes

--- Shablonproject/flask-oracle-project/proj_v0_01.py
+++ Shablonproject/flask-oracle-project/proj_v0_01.py
@@ -1,6 +1,7 @@
 import cx_Oracle
 from flask import Flask, request, session, render_template, redirect, url_for
 from hashlib import sha256
+from datetime import datetime, date, time
 
 try:
     conn = cx_Oracle.connect('super/abcd1234@//localhost:1521/orcl')
@@ -41,6 +42,35 @@ def select_from_users_where(select, where, *args):
 		cur.close()
 	return users
 
+def select_all_from_product():
+	try:
+		cur = conn.cursor()
+		sql_select = "SELECT * FROM PRODUCT"	
+		cur.execute(sql_select)
+		products = cur.fetchall()
+	except Exception as err:
+		print('Exception occured while fetching the records ', err)
+	else:
+		print('Query Completed.')
+	finally:
+		cur.close()
+	return products
+
+def select_product_from_products(product, *args):
+	try:
+		cur = conn.cursor()
+		sql_select = f"SELECT * FROM PRODUCT WHERE {product}"
+		data = tuple(args)
+		cur.execute(sql_select, data)
+		product = cur.fetchall()
+	except Exception as err:
+		print('Exception occured while fetching the records ', err)
+	else:
+		print('Query Completed.')
+	finally:
+		cur.close()
+	return product
+
 def insert_into_users(columns, *args):
 	email, password, first_name, last_name = args
 	values = ":1"
@@ -63,28 +93,69 @@ def insert_into_users(columns, *args):
 	finally:
 		cur.close()
 
+def insert_into_product(merchant_id, product_name, product_price, product_image_url, product_description):
+	err = []
+	try:
+		cur = conn.cursor()
+		data = (merchant_id, product_name, product_price, product_image_url, product_description)
+		cur.callproc('insert_into_product', data)
+	except cx_Oracle.IntegrityError as e:
+		errorObj, = e.args
+		err.append("ERROR: " + str(errorObj))
+		print('ERROR while inserting the data ', errorObj)
+	else:
+		print('Insert Completed.')
+		conn.commit()
+	finally:
+		cur.close()
+	return err	
+		
+def insert_into_merchants(merchant_name, admin_id):
+	try:
+		cur = conn.cursor()
+		sql_insert = f"insert into merchants values(merchId.nextval, '{merchant_name}', {admin_id}, sysdate)"
+		print(sql_insert)
+		cur.execute(sql_insert)
+	except cx_Oracle.IntegrityError as e:
+		errorObj, = e.args
+		print('ERROR while inserting the data ', errorObj)
+		err.append("Merchant already exists.")
+	else:
+		print('Insert Completed.')
+		conn.commit()
+	finally:
+		cur.close()
 
+def select_adminId_from_merchants(user_id, *args):
+	try:
+		cur = conn.cursor()
+		sql_select = f"SELECT id FROM MERCHANTS WHERE {user_id}"
+		data = tuple(args)
+		cur.execute(sql_select, data)
+		merchant_admin_id = cur.fetchall()
+	except Exception as err:
+		print('Exception occured while fetching the records ', err)
+	else:
+		print('Query Completed.')
+	finally:
+		cur.close()
+	return merchant_admin_id
 
-	
-# @app.route('/')
-# @app.route('/home')
-# def index():
-# 	err = []
-# 	if session['email'] is not None and session['password'] is not None:
-# 		email = session['email']
-# 		password = session['password']
-# 		users = select_from_users_where(
-# 			"id, email, first_name, last_name", 
-# 			"email=:1 AND password=:2", 
-# 			email, password
-# 		)
-# 		if len(users) > 0:
-# 			return render_template('home.html', users=users)
-# 		else:
-# 			err.append("Username OR password is incorrect.")
-# 			return render_template('login.html', errors=err)
-# 	else:
-# 		return redirect('/login')
+def select_merchantId_from_merchants(admin_id, *args):
+	try:
+		cur = conn.cursor()
+		sql_select = f"SELECT id FROM MERCHANTS WHERE {admin_id}"
+		data = tuple(args)
+		cur.execute(sql_select, data)
+		merchant_id = cur.fetchall()
+	except Exception as err:
+		print('Exception occured while fetching the records ', err)
+	else:
+		print('Query Completed.')
+	finally:
+		cur.close()
+	return merchant_id
+
 @app.route('/register', methods=['POST', 'GET'])
 def register():
 	err = []
@@ -102,13 +173,66 @@ def register():
 		users = select_from_users('id, email, first_name, last_name')
 		return render_template('register.html', users=users)
 
+
+
 @app.route('/')
 def index():
-	if session['email'] == None:
-		return render_template('login.html')
+	if request.method== 'GET':
+		if 'email' in session:
+			products = select_all_from_product()
+			return render_template('home.html', user=session['email'], products=products)
+		else: 
+			return redirect('/login')
+
+@app.route('/product/delete/')
+
+@app.route('/product/<int:product_id>')
+def product_detail(product_id):
+	if request.method == 'GET':
+		product = select_product_from_products('id=:1' ,product_id)
+		user_id = select_from_users_where('id', 'email=:1',session['email'])
+		merchant_id = select_merchantId_from_merchants('admin_id=:1', user_id[0][0])[0][0]
+		if merchant_id!=[]:
+			merchant_id_of_product = product[0][1]
+			print(merchant_id_of_product)
+			print(merchant_id)
+			if merchant_id_of_product == merchant_id:
+				
+				return render_template('product_detail.html', product = product, owner=1)
+		
+	
+		
+	return render_template('product_detail.html', product = product, owner=0)
+	
+@app.route('/product/create', methods=['POST', 'GET'])
+def create_product():
+	if request.method == 'POST':
+		if session['email'] :
+			product_name = request.form['name']
+			product_description = request.form['description']
+			product_price = request.form['price']
+			product_image_url = request.form['image_url']
+			
+			user_id = select_from_users_where('id', 'email=:1',session['email'])
+			user_id = user_id[0][0]
+			print(user_id)
+			merchant_id = select_merchantId_from_merchants('admin_id=:1', user_id)
+			
+
+			print(merchant_id)
+			if merchant_id ==[]:
+				user_name = select_from_users_where('first_name', 'email=:1',session['email'])[0][0]
+				print(user_name)
+				insert_into_merchants(user_name, user_id)
+			
+			insert_into_product(merchant_id[0][0], product_name, product_price, product_image_url, product_description)
+
+			return redirect('/')
+		else:
+			return redirect('/login')
 	else:
-		print(session['email'])
-		return render_template('home.html', user=session['email'])
+		return render_template('create_product.html')
+
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
